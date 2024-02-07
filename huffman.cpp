7dfc6ee3082ac095f6d2579e62310b6dcb1137bb
Stharley <sstharley@gmail.com>
2024-02-07 01:57:15 -0300

    some improvements

--- huffman.cpp
+++ huffman.cpp
@@ -1,22 +1,29 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <map>
+#include <vector>
+
 
 using namespace std;
 
+string file_name = "input.txt";
+
 struct Node {
-    unsigned char caracter;
-    unsigned int freq;
+    unsigned char character;
+    unsigned int frequency;
     struct Node * right;
     struct Node * left;
-    Node(unsigned char c, unsigned int f, Node* r = nullptr, Node* l = nullptr) : caracter(c), freq(f), right(r), left(l) {}
+    Node(unsigned char c, unsigned int f, Node* r = nullptr, Node* l = nullptr) : character(c), frequency(f), right(r), left(l) {}
     // Operador de comparação para a priority_queue, usando > para uma pq de menor para maior frequência
-    bool operator<(const Node& outro) const { return freq > outro.freq; }
+    bool operator<(const Node& outro) const { return frequency > outro.frequency; }
 };
 
 Node * build_tree_from_priority_queue(priority_queue<Node>& pq) {
     while(pq.size() > 1) {
         Node* first = new Node(pq.top()); pq.pop(); 
         Node* second = new Node(pq.top()); pq.pop();
-        Node* parent = new Node(NULL, first->freq + second->freq, first, second);
+        Node* parent = new Node(NULL, first->frequency + second->frequency, first, second);
         pq.push(*parent);
     } 
     return new Node(pq.top()); 
@@ -24,9 +31,16 @@ Node * build_tree_from_priority_queue(priority_queue<Node>& pq) {
 
 void create_dictionary (map<char, string>& dictionary, Node * node, string path) { 
     // quando chegar nos nós
-    if(!node->left and !node->right) dictionary[node->caracter] = path;
-    if(node->left ) create_dictionary(dictionary, node->left , path+"0");
-    if(node->right) create_dictionary(dictionary, node->right, path+"1");
+    if(!node->left and !node->right) dictionary[node->character] = path;
+    if(node->left) create_dictionary(dictionary, node->left , path + "0");
+    if(node->right) create_dictionary(dictionary, node->right, path + "1");
+}
+
+void print_dictionary (map<char, string> & dictionary) {
+    cout << "Dicionario: " << endl;
+    for(const auto& pair: dictionary) {
+       cout << pair.first << ": " << pair.second << endl; 
+    }
 }
 
 string encode (string text, map<char, string>& dictionary) {
@@ -42,7 +56,7 @@ string decode (string code, Node * root) {
         if(x == '0') aux = aux->left;
         else         aux = aux->right; 
         if(!aux->left and !aux->right){
-            text += aux->caracter;
+            text += aux->character;
             aux = root;
         }
     }
@@ -50,46 +64,42 @@ string decode (string code, Node * root) {
 }
 
 void compress(string code) {
-    fstream arquivo("output.txt", ios::out);
+    fstream file(file_name+".huff", ios::out);
     unsigned char byte = 0;
-
-    string content;
 
     int j = 7; 
     
     for(auto & bit: code) {
-        unsigned char mascara = 1;
+        unsigned char mask = 1;
         
         if (bit == '1') {
-            mascara = mascara << j;
-            byte = byte | mascara;
+            mask = mask << j;
+            byte = byte | mask;
         }
 
         j--;
 
         if (j < 0) {
-            content += byte;
+            file << byte;
             byte = 0;
             j = 7;
         }
 
     }
 
-    if (j != 7) content += byte;
+    if (j != 7) file << byte;
     
-    arquivo << content;
-
-    arquivo.close();
+    file.close();
 }
 
 unsigned int bit_is_set(unsigned char byte, int i) {
-  unsigned char mascara = (1 << i);
-  return byte & mascara;
+    unsigned char mask = (1 << i);
+    return byte & mask;
 }
 
 string decompress(Node * root) {
 
-    ifstream file("output.txt"); // abre o arquivo em modo leitura
+    ifstream file(file_name+".huff"); // abre o arquivo em modo leitura
 
     string text; 
     Node * aux = root;
@@ -100,7 +110,7 @@ string decompress(Node * root) {
             if(bit_is_set(byte, i)) aux = aux->right;
             else                    aux = aux->left;
             if(!aux->left and !aux->right){
-                text+= aux->caracter;
+                text+= aux->character;
                 aux = root;
             };
         }
@@ -110,27 +120,33 @@ string decompress(Node * root) {
     return text;
 }
 
-int main(void) {
+int main(int argc, char ** argv) {
  
-    string nomeArquivo = "input.txt";
-    ifstream arquivo(nomeArquivo); // abre o arquivo em modo leitura
+    if(argc >= 2) file_name = argv[1];
 
-    vector<int> tabela_frequencia(257,0);
+    ifstream file(file_name); // abre o arquivo em modo leitura
+
+    if(!file) {
+        cout << "Erro ao abrir o arquivo: " << file_name << endl;
+        exit(1);
+    }
+                                
+    vector<int> frequency_table(257,0);
     
     string text;
 
-    char caractere; // para guardar cada caractere do arquivo
-    while (arquivo.get(caractere)) {
-        text+=caractere;
-        tabela_frequencia[caractere]++;
+    char c; 
+    while (file.get(c)) {
+        text +=c ;
+        frequency_table[c]++;
     }
 
-    arquivo.close();
+    file.close();
 
     // fila de prioridade
     priority_queue<Node> pq;
-    for(int i = 0; i<257; ++i) if(tabela_frequencia[i]) {
-        Node no(i,tabela_frequencia[i]);
+    for(int i = 0; i<257; ++i) if(frequency_table[i]) {
+        Node no(i,frequency_table[i]);
         pq.push(no);
     }
 
@@ -140,12 +156,9 @@ int main(void) {
     // dicionario 
     map<char,string> dictionary;
     create_dictionary(dictionary, tree,"");
+    print_dictionary(dictionary);
 
-    // TODO: Compactacao e Descompactacao
-
-    //cout << decode(encode(text,dictionary),tree) << endl; 
     string code = encode(text, dictionary);
-
 
     compress(code);
 
